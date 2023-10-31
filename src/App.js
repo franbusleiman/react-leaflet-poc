8f54e201f0620e7b37aef58aee222240aa1551d4
franbusleiman <franbusleiman@gmail.com>
2023-10-31 00:25:45 -0300

    Tile Layer test App.js

--- src/App.js
+++ src/App.js
@@ -30,10 +30,7 @@ const App = () => {
   return (
     <MapContainer center={position} zoom={Zoom} style={{ height: '1000px', width: '100%' }}>
       <MapEvents />
-      <TileLayer
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-        attribution='<a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+     
 
       {(Zoom > 10) &&
 
