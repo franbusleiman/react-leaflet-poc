bc286e89285a651fc109594e2281b0cc75a44fe3
franbusleiman <franbusleiman@gmail.com>
2024-01-13 19:32:53 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const App = () => {
       <MapEvents />
 
       <TileLayer
-       url= 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}
+       url= 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}'
 
        	attribution= '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
        	ext= 'png'
