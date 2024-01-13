ec6f911189b41b56c86c8d6101cf7893e2bc14c8
franbusleiman <franbusleiman@gmail.com>
2024-01-13 19:05:35 -0300

    change initial location and changed tilelayer

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const App = () => {
       <MapEvents />
 
       <TileLayer
-       url= 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}'
+       url= 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}'
 
        	attribution= '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
        	ext= 'png'
