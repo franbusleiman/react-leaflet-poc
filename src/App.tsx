2f68c11f10655c7ee62e69f9e329fa93ab5247f9
franbusleiman <franbusleiman@gmail.com>
2024-01-13 22:14:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,9 +45,9 @@ const App = () => {
       <MapEvents />
 
       <TileLayer
-       url= 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+       url= 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}'
 
-       	attribution= 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+       	attribution= '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
        	ext= 'png'
       />
 
